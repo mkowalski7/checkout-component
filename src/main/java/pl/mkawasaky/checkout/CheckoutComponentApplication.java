b22c7b58a652213d70992a75743d01c31c94c585
OSZKI <milosz.priv@gmail.com>
2025-09-13 15:08:28 +0200

    Initial commit

---
+++ src/main/java/pl/mkawasaky/checkout/CheckoutComponentApplication.java
@@ -0,0 +1,13 @@
+package pl.mkawasaky.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutComponentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mkawasaky/checkout/CheckoutComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mkawasaky.checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutComponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
