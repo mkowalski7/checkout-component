5f4bfc15edf12f2a31b2236f378f851683ef05ba
OSZKI <milosz.priv@gmail.com>
2025-09-16 14:31:58 +0200

    feat: add product model and repository structure

---
+++ src/main/java/pl/checkout/model/Product.java
@@ -0,0 +1,33 @@
+package pl.checkout.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Table(name = "products")
+@Entity
+@Data
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, length = 20)
+    @NotEmpty
+    private String sku;
+
+    @Column
+    @NotEmpty
+    private String name;
+
+    @Column(precision = 10, scale = 2)
+    @Positive
+    private BigDecimal price;
+}
---
+++ src/main/java/pl/checkout/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package pl.checkout.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.checkout.model.Product;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+    Optional<Product> findBySKU(String sku);
+}
